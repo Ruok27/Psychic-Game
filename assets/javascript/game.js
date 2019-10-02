741b65a0f91852175114b39cf8fee2632563079a
ruok27 <jakxii27@gmail.com>
2019-10-02 17:20:35 -0400

    Layout of assignment done with leftover test js numbers displayed

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+document.getElementById("win-count").innerHTML = "43";
+document.getElementById("lose-count").innerHTML = "88";
+document.getElementById("guess-left").innerHTML = "4";
+document.getElementById("guess-count").innerHTML = "1111113";
