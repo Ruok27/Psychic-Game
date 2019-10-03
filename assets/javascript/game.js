128c72ec70a7a877c05a887ce8665e7c49c11c73
ruok27 <jakxii27@gmail.com>
2019-10-03 18:12:33 -0400

    added win and lose conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,20 @@
-document.getElementById("win-count").innerHTML = "43";
-document.getElementById("lose-count").innerHTML = "88";
-document.getElementById("guess-left").innerHTML = "4";
-document.getElementById("guess-count").innerHTML = "1111113";
+
+
+
+
 
 let letters = [];
-//This literally took 10 minutes for me to figure out cause i wanted a quicker way to set up an alphabet array instead of typing it all in
+let winCount = 0;
+let loseCount = 0;
+let guessCount = 10;
+let guessLetters = 0;
 
+
+
+document.onkeydown = function(event){
+let input = event.key.toLowerCase();
+
+//This literally took 10 minutes for me to figure out cause i wanted a quicker way to set up an alphabet array instead of typing it all in
 for (let i = 97; i < 123;i++){
     
     letters.push(String.fromCharCode(i));
@@ -14,7 +23,29 @@ for (let i = 97; i < 123;i++){
 
 let cpuChoice = letters[Math.floor(Math.random() * (25 - 0 + 1)) + 0];
 
+if (guessCount==0){
+    guessLetters = " ";
+    loseCount++;
+    guessCount +=11;
+    document.getElementById("lose-count").innerHTML = loseCount;
+        
+    }
 
+
+if (input === cpuChoice){
+winCount++;
+document.getElementById("win-count").innerHTML = winCount;
+console.log("You won! " + input + " " + cpuChoice);
+}
+
+else 
+guessCount--;
+guessLetters += " "+ input; 
+console.log(input + " " + cpuChoice);
+document.getElementById("guess-left").innerHTML = guessCount;
+document.getElementById("guess-letters").innerHTML = guessLetters;
+
+}
 
 
 
